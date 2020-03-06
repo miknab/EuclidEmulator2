e989d08ce4c1c20bd9fc7d2a6186625af68d362a
Mischa Knabenhans <mischak@physik.uzh.ch>
2020-03-06 14:18:36 +0100

    Memory mapping data file seems to syntactically work

--- src/emulator.cxx
+++ src/emulator.cxx
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <string>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include "cosmology.h"
 #include "pce.h"
 #include "emulator.h"
 
+using namespace std; 
+
+/* CONSTRUCTOR */
 EuclidEmulator::EuclidEmulator(): 
 	nz(101),
 	nk(613),
-	nCoeffs{53, 53, 117, 117, 53, \
+	n_coeffs{53, 53, 117, 117, 53, \
 			117, 117, 117, 117, 521, \
 			117, 1539, 173, 457}
-{
-	/* === Member function === */
-	// Constructor loading PCE data file
+{	
+	/// VARIABLE DECLARATIONS ///
+	off_t size;
+    struct stat s;
+	double *data;
 
+	// ==== LOAD EUCLIDEMULATOR2 DATA FILE ==== //
+	int fp = open("./ee2_bindata.dat", O_RDONLY);
+	if(!fp) {
+		cerr << "Unable to open ./ee2_data/ee2_pcs.dat.\n";
+        exit(1);
+	}
+
+	// Get the size of the file. //
+    printf("Get file size...\n");
+    int status = fstat(fp, & s);
+    size = s.st_size;
+
+	// Map the file into memory //
+	data = (double *) mmap (0, size, PROT_READ, MAP_PRIVATE, fp, 0);
 }
 
 // Compute NLC
