eadb2c22303d671378289d27bd4e61de4648bf14
Mischa Knabenhans <mischak@physik.uzh.ch>
2020-03-06 20:24:12 +0100

    Parameter range checker implemented

--- src/cosmology.cxx
+++ src/cosmology.cxx
@@ -1,8 +1,30 @@
+#include <assert.h>
 #include "cosmology.h"
 
-Cosmology::Cosmology(){}
+/* CONSTRUCTOR */
+Cosmology::Cosmology(double Omega_b, double Omega_m, double Sum_m_nu, double n_s, double h, double w_0, double w_a, double A_s){
+	this->cosmo[0] = Omega_b;
+    this->cosmo[1] = Omega_m;
+    this->cosmo[2] = Sum_m_nu;
+    this->cosmo[3] = n_s;
+    this->cosmo[4] = h;
+    this->cosmo[5] = w_0;
+    this->cosmo[6] = w_a;
+	this->cosmo[7] = A_s;
+}
 
-void Cosmology::read_from_file(char *filename){}
+/* CHECK PARAMETER RANGES */
+void Cosmology::check_parameter_ranges(){
+    for(int i=0 ; i<8; ++i) {
+        assert( this->cosmo[i] >= this->minima[i] && \
+			    this->cosmo[i] <= this->maxima[i] );
+    }
+}
 
-void Cosmology::read_csm(){}
+/* ISOPROBALISTIC TRANSFORMATION TO UNIT HYPERCUBE */
+void Cosmology::isoprob_tf(){
+	for (int i=0; i<8; ++i){}
+}
+/* READ COSMOLOGY FROM FILE  */
+void read_from_file(char *filename){}
 
--- src/cosmology.h
+++ src/cosmology.h
@@ -3,18 +3,20 @@
 
 class Cosmology{
 	public:
-		double Omega_b;
-	    double Omega_m;
-		double Sum_m_nu;
-    	double n_s;
-    	double h;
-    	double w_0;
-		double w_a;
-    	double A_s;
+		double cosmo[8];
 
-		Cosmology();
+		/* Ranges for cosmological parameters */
+        const double minima[8] = {0.04, 0.24, 0.00, 0.92, 0.61, -1.3, -0.7, 1.7e-9};
+        const double maxima[8] = {0.06, 0.40, 0.15, 1.00, 0.73, -0.7, 0.5, 2.5e-9};
+		
+		/* Member functions */
+		Cosmology(double Omega_b, double Omega_m, double Sum_m_nu, double n_s, double h, double w_0, double w_a, double A_s);
 		void read_from_file(char *filename);
-		void read_csm();
+
+	private:
+		double cosmo_tf[8];
+		void isoprob_tf();
+		void check_parameter_ranges();
 };
 
 #endif
