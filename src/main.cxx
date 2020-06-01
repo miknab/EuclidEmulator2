bd7956b5ebbbd9471f6debfe2c8f05f8cb8c022a
Mischa Knabenhans <mischak@physik.uzh.ch>
2020-06-01 19:14:14 +0200

    Added more output for debugging...

--- src/main.cxx
+++ src/main.cxx
@@ -18,22 +18,44 @@ int main(int argc, char *argv[]) {
 	/* GET INPUT PARAMETERS */
 	CSM = ee2_parser(argc, argv);
 	n_cosmologies = CSM.Omega_b.size();
-	std::cout << "There are " << n_cosmologies << " cosmologies specified in the input." << std::endl;
+	std::cout << "\nThere is/are " << n_cosmologies << " cosmologies specified in the input." << std::endl;
 
 	/* INITIALIZE EE2 SESSION */
     EuclidEmulator ee2 = EuclidEmulator();
-    std::cout << "EuclidEmulator2 >> Session started... " << std::endl;
+    std::cout << "\nEuclidEmulator2 >> Session started... " << std::endl;
 
 	/* ITERATE THROUGH COSMOLOGIES */
 	for (int cntr = 0; cntr < n_cosmologies; cntr++){
 		/* Instatiate the Cosmology class */
+		/*printf("Omega_b: ");
+		printf("Omega_b[%d] = %f\n", cntr, CSM.Omega_b[cntr]);
+		printf("Omega_m: ");
+		printf("Omega_m[%d] = %f\n", cntr, CSM.Omega_m[cntr]);
+		printf("Sum_m_nu: ");
+		printf("Sum_m_nu[%d] = %f\n", cntr, CSM.Sum_m_nu[cntr]);
+		printf("n_s: ");
+		printf("n_s[%d] = %f\n", cntr, CSM.n_s[cntr]);
+		printf("h: ");
+		printf("h[%d] = %f\n", cntr, CSM.h[cntr]);
+		printf("w_0: ");
+		printf("w_0[%d] = %f\n", cntr, CSM.w_0[cntr]);
+		printf("w_a: ");
+		printf("w_a[%d] = %f\n", cntr, CSM.w_a[cntr]);
+		printf("A_s: ");
+		printf("A_s[%d] = %.5e\n", cntr, CSM.A_s[cntr]);
+		printf("Entering cosmology class..\n");
+		*/
 		Cosmology cosmo = Cosmology(CSM.Omega_b[cntr], CSM.Omega_m[cntr], \
 								    CSM.Sum_m_nu[cntr], CSM.n_s[cntr], CSM.h[cntr], \
 									CSM.w_0[cntr], CSM.w_a[cntr], CSM.A_s[cntr]);
 
+		//printf("Cosmology struct created successfully\n");
 		/* compute NLCs for each cosmology */
+		/*for(int i = 0; i < CSM.n_redshift[cntr]; i++){
+			printf("z[%d][%d] = %f\n", cntr, i, CSM.zvec.at(cntr).at(i));
+		}*/
 		ee2.compute_nlc(cosmo, CSM.zvec[cntr], CSM.n_redshift[cntr]);
-
+		//printf("NLC computed successfully.\n");
 		/* Write result to file. Format: k [h/Mpc] B(k,z0) B(k,z1) ... B(k,zn) */
 		string filename = "my_nlc"+to_string(cntr)+".dat";
 		ee2.write_nlc2file(filename, CSM.zvec[cntr], CSM.n_redshift[cntr]);
