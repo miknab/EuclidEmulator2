0b2266697929f48c0badbcc777fec48c2b29891b
Mischa Knabenhans <mischak@physik.uzh.ch>
2020-10-06 00:50:39 +0200

    Added license header to all relevant files

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,3 +1,21 @@
+# Test script for EuclidEmulator2
+# ===============================
+# This file is part of EuclidEmulator2 
+# Copyright (c) 2020 Mischa Knabenhans
+#
+# EuclidEmulator2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EuclidEmulator2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/bin/bash
 ./ee2.exe -b 4.18613219e-2\
 		  -m 2.88868201e-1\
