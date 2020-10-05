7567d3fe79663fbd3684490385fe49066085d278
Mischa Knabenhans <mischak@physik.uzh.ch>
2020-10-06 00:05:25 +0200

    *.ini and run_test.sh modified such that all test results are written into results directory (and not in subdirs thereof)

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -8,12 +8,8 @@
 		  -w -2.20396591e-1\
 		  -A 2.43811423e-9\
 		  -z 0.0 -z 0.1081179 -z 0.4880429 -z 1.010451 -z 1.998261 -z 3.035232 -z 5.255458 -z 10.0\
-		  -o cmd_input_test.dat
+		  -o cmd_input_test
 
 ./ee2.exe -p tests/ee2_parfile.par -o from_ee2_parfile
-
-mkdir results/CAMBpath
-./ee2.exe -i tests/CAMB_parfile.ini -d results/CAMBpath -t CAMB
-
-mkdir results/CLASSpath
-./ee2.exe -i tests/CLASS_parfile.ini -d results/CLASSpath -t CLASS
+./ee2.exe -i tests/CAMB_parfile.ini -d results -t CAMB
+./ee2.exe -i tests/CLASS_parfile.ini -d results -t CLASS
